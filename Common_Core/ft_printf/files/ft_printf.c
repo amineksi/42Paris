05961ef733b4bed0ef014cd632c591e8af7b0ff8
amineksi <amine.kass35@gmail.com>
2024-08-06 19:58:06 +0200

    Add %

--- Common_Core/ft_printf/files/ft_printf.c
+++ Common_Core/ft_printf/files/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: amine <amine@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/05 22:22:04 by amine             #+#    #+#             */
-/*   Updated: 2024/08/06 18:57:08 by amine            ###   ########.fr       */
+/*   Updated: 2024/08/06 19:21:32 by amine            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,8 @@ int	processing(const char *str, va_list args, int i)
 		return (ft_printi(va_arg(args, int)));
 	else if (str[i] == 'u')
 		return (ft_printu(va_arg(args, unsigned int)));
+	else if (str[i] == '%')
+		return (ft_printc('%'));
 	return (0);
 }
 
