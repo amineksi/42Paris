7551421eaad9da5c9e74c2396dbafd1dcbc6ab82
Amine <amine.kass35@gmail.com>
2024-09-21 01:13:33 +0200

    minitalk supposedly finished, need to add libft and fix few things

--- Common_Core/minitalk/client.c
+++ Common_Core/minitalk/client.c
@@ -6,8 +6,42 @@
 /*   By: amine <amine@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 18:06:27 by amine             #+#    #+#             */
-/*   Updated: 2024/09/20 18:06:52 by amine            ###   ########.fr       */
+/*   Updated: 2024/09/21 01:12:31 by amine            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main()
+#include "minitalk.h"
+
+void    send_signal(long int pid, char c)
+{
+    int				i;
+	unsigned char	tmp;
+
+	i = 7;
+	while (i != -1)
+	{
+		tmp = c >> i;
+		if (tmp % 2 == 0)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		i--;
+		usleep(100);
+	}
+	printf("\n");
+}
+
+int main(int argc, char *argv[])
+{
+	int			i;
+	pid_t		pid;
+	const char	*msg;
+	
+	i = 0;
+	pid = atoi(argv[1]);
+	msg = argv[2];
+	while (msg[i])
+		send_signal(pid, msg[i++]);
+	send_signal(pid, '\0');
+	return (0);
+}
--- Common_Core/minitalk/server.c
+++ Common_Core/minitalk/server.c
@@ -6,15 +6,38 @@
 /*   By: amine <amine@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 14:21:46 by amine             #+#    #+#             */
-/*   Updated: 2024/09/20 14:23:09 by amine            ###   ########.fr       */
+/*   Updated: 2024/09/21 01:12:49 by amine            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <unistd.h>
+#include "minitalk.h"
+
+void	signalHandler(int sig)
+{
+	static	unsigned char	received_byte;
+	static	int				bit_position;
+	
+	received_byte |= (sig == SIGUSR2);
+	bit_position++;
+	if (bit_position == 8)
+	{
+		if (received_byte == '\0')
+			write(1, "\n", 1);
+		else
+			printf("%c", received_byte);
+		bit_position = 0;
+		received_byte = 0;
+	} 
+	else
+		received_byte <<= 1;
+}
 
 int main(void)
 {
-    printf("%ld", (long)getpid());
+	printf("%d\n", getpid());
+    signal(SIGUSR1, signalHandler);
+	signal(SIGUSR2, signalHandler);
+	while (1)
+		pause();
     return 0;
 }
